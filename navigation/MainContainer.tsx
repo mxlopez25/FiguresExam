fd670baa8a88cb76b36a3556c0032179c6f3d97e
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-10-13 16:29:52 -0400

    Base added

---
+++ navigation/MainContainer.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { View, Text } from 'react-native'
+
+export default function MainContainer() {
+    return(
+        <View><Text>Main Container</Text></View>
+    );
+}
