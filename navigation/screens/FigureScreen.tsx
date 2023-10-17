1e6586807a44a26c7e5ced6ee8d03d90fda268e7
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-10-17 11:45:07 -0400

    FiguresScreen update

--- navigation/screens/FigureScreen.tsx
+++ navigation/screens/FigureScreen.tsx
@@ -7,33 +7,77 @@ import {
   TextInput,
   StyleSheet,
   Dimensions,
+  Button,
+  Pressable,
 } from "react-native";
 
 const win = Dimensions.get("window");
 
 export default function FigureScreen() {
+  const [figure1a, setFigure1A] = React.useState("0"); // Figure 1 value to Calculate
+  const [figure1b, setFigure1B] = React.useState("0");
+  const [figure1c, setFigure1C] = React.useState("0");
 
-  const [figure1a, setFigure1A] = React.useState(''); // Figure 1 value to Calculate
-  const [figure1b, setFigure1B] = React.useState('');
-  const [figure1c, setFigure1C] = React.useState('');
+  const [figure2a, setFigure2A] = React.useState("0");
+  const [figure2b, setFigure2B] = React.useState("0");
+  const [figure2c, setFigure2C] = React.useState("0");
+  const [figure2d, setFigure2D] = React.useState("0");
+  const [figure2e, setFigure2E] = React.useState("0");
+  const [figure2f, setFigure2F] = React.useState("0");
+  const [figure2g, setFigure2G] = React.useState("0"); // Figure 2 value to Calculate
 
-  const [figure2a, setFigure2A] = React.useState('');
-  const [figure2b, setFigure2B] = React.useState('');
-  const [figure2c, setFigure2C] = React.useState('');
-  const [figure2d, setFigure2D] = React.useState('');
-  const [figure2e, setFigure2E] = React.useState('');
-  const [figure2f, setFigure2F] = React.useState('');
-  const [figure2g, setFigure2G] = React.useState(''); // Figure 2 value to Calculate
+  const [figure3a, setFigure3A] = React.useState("0"); // Figure 3 value to Calculate
+  const [figure3b, setFigure3B] = React.useState("0");
+  const [figure3c, setFigure3C] = React.useState("0");
+  const [figure3d, setFigure3D] = React.useState("0");
 
-  const [figure3a, setFigure3A] = React.useState(''); // Figure 3 value to Calculate
-  const [figure3b, setFigure3B] = React.useState('');
-  const [figure3c, setFigure3C] = React.useState('');
-  const [figure3d, setFigure3D] = React.useState('');
+  function ValidateNumber(value) {
+    return !isNaN(value) && value > 0;
+  }
+
+  function pythagorean(sideA, sideB) {
+    return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+  }
+
+  function validateFigure1A() {
+    if (ValidateNumber(figure1b) && ValidateNumber(figure1c)) {
+      console.log(figure1b);
+      console.log(figure1c);
+      let result = pythagorean(parseFloat(figure1b), parseFloat(figure1c));
+      console.log(result);
+      setFigure1A(result.toFixed(5).toString());
+    } else {
+      setFigure1A("00");
+    }
+  }
+
+  function validateFigure2G() {
+    if (figure1b.length > 0 && figure1c.length > 0) {
+      setFigure2G("01");
+    } else {
+      setFigure2G("00");
+    }
+  }
+
+  function validateFigure3A() {
+    if (figure1b.length > 0 && figure1c.length > 0) {
+      setFigure2G("01");
+    } else {
+      setFigure2G("00");
+    }
+  }
 
   return (
     <ScrollView style={styles.figuresContainer}>
-      <View style={{ flex: 1 }}>
-        <Text>Figure: Figure 1</Text>
+      <View>
+        <View style={styles.figuresTopOptions}>
+          <Text>
+            Figure
+          </Text>
+          <Pressable style={{borderColor: '#000000', borderRadius: 4}}>
+            <Text>Pick Figure</Text>
+          </Pressable>
+        </View>
         <View style={styles.imageContainer}>
           <Image
             style={styles.figuresImages}
@@ -41,21 +85,12 @@ export default function FigureScreen() {
           />
         </View>
         <Text>Measurements</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={setFigure1A}
-          value={figure1a}
-          editable={false}
-        />
+        <TextInput style={styles.input} value={figure1a} editable={false} />
         <TextInput
           style={styles.input}
           onChangeText={(value) => {
             setFigure1B(value);
-            if(figure1b.length > 0 && figure1c.length > 0) {
-                setFigure1A('01');
-            } else {
-                setFigure1A('00');
-            }
+            validateFigure1A();
           }}
           value={figure1b}
         />
@@ -63,17 +98,20 @@ export default function FigureScreen() {
           style={styles.input}
           onChangeText={(value) => {
             setFigure1C(value);
-            if(figure1b.length > 0 && figure1c.length > 0) {
-                setFigure1A('01');
-            } else {
-                setFigure1A('00');
-            }
+            validateFigure1A();
           }}
           value={figure1c}
         />
       </View>
       <View style={{ flex: 1 }}>
-        <Text>Figure: Figure 2</Text>
+      <View style={styles.figuresTopOptions}>
+          <Text>
+            Figure
+          </Text>
+          <Pressable style={{borderColor: '#000000', borderRadius: 4}}>
+            <Text>Pick Figure</Text>
+          </Pressable>
+        </View>
         <View style={styles.imageContainer}>
           <Image
             style={styles.figuresImages}
@@ -83,43 +121,63 @@ export default function FigureScreen() {
         <Text>Measurements</Text>
         <TextInput
           style={styles.input}
-          onChangeText={setFigure2A}
+          onChangeText={(value) => {
+            setFigure2A(value);
+            validateFigure2G();
+          }}
           value={figure2a}
         />
         <TextInput
           style={styles.input}
-          onChangeText={setFigure2B}
+          onChangeText={(value) => {
+            setFigure2B(value);
+            validateFigure2G();
+          }}
           value={figure2b}
         />
         <TextInput
           style={styles.input}
-          onChangeText={setFigure2C}
+          onChangeText={(value) => {
+            setFigure2C(value);
+            validateFigure2G();
+          }}
           value={figure2c}
         />
         <TextInput
           style={styles.input}
-          onChangeText={setFigure2D}
+          onChangeText={(value) => {
+            setFigure2D(value);
+            validateFigure2G();
+          }}
           value={figure2d}
         />
         <TextInput
           style={styles.input}
-          onChangeText={setFigure2E}
+          onChangeText={(value) => {
+            setFigure2E(value);
+            validateFigure2G();
+          }}
           value={figure2e}
         />
         <TextInput
           style={styles.input}
-          onChangeText={setFigure2F}
+          onChangeText={(value) => {
+            setFigure2F(value);
+            validateFigure2G();
+          }}
           value={figure2f}
         />
-        <TextInput
-          style={styles.input}
-          onChangeText={setFigure2G}
-          value={figure2g}
-          editable={false}
-        />
+        <TextInput style={styles.input} value={figure2g} editable={false} />
       </View>
       <View style={{ flex: 1 }}>
-        <Text>Figure: Figure 3</Text>
+      <View style={styles.figuresTopOptions}>
+          <Text>
+            Figure
+          </Text>
+          <Pressable style={{borderColor: '#000000', borderRadius: 4}}>
+            <Text>Pick Figure</Text>
+          </Pressable>
+        </View>
         <View style={styles.imageContainer}>
           <Image
             style={styles.figuresImages}
@@ -134,17 +192,26 @@ export default function FigureScreen() {
         />
         <TextInput
           style={styles.input}
-          onChangeText={setFigure3B}
+          onChangeText={(value) => {
+            setFigure3B(value);
+            validateFigure3A();
+          }}
           value={figure3b}
         />
         <TextInput
           style={styles.input}
-          onChangeText={setFigure3C}
+          onChangeText={(value) => {
+            setFigure3C(value);
+            validateFigure3A();
+          }}
           value={figure3c}
         />
         <TextInput
           style={styles.input}
-          onChangeText={setFigure3D}
+          onChangeText={(value) => {
+            setFigure3D(value);
+            validateFigure3A();
+          }}
           value={figure3d}
         />
       </View>
@@ -153,11 +220,12 @@ export default function FigureScreen() {
 }
 
 const styles = StyleSheet.create({
+  figuresTopOptions: {flexDirection: 'row', justifyContent: 'space-around'},
   imageContainer: {
     width: win.width,
     height: 200,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   figuresImages: {
     height: 200,
