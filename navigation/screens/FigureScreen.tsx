065ad9f5bc9bb177cd6643ca3452bcecdaeda06a
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-10-27 00:43:46 -0400

    FigureScreen updated

--- navigation/screens/FigureScreen.tsx
+++ navigation/screens/FigureScreen.tsx
@@ -11,31 +11,29 @@ import {
   Pressable,
 } from "react-native";
 import { FiguresContext } from "../MainContainer";
-
-
 const win = Dimensions.get("window");
 
 export default function FigureScreen() {
   const figures = React.useContext(FiguresContext);
 
   React.useEffect(() => {
-    if(ValidateNumber(figures.figure1c) && ValidateNumber(figures.figure1b)) {
+    if (ValidateNumber(figures.figure1c) && ValidateNumber(figures.figure1b)) {
       const s1 = Number(figures.figure1b);
       const s2 = Number(figures.figure1c);
       const r = pythagoreanAlt(s2, s1);
-      figures.setFigure1AData('' + r.toFixed(2));
+      figures.setFigure1AData("" + r.toFixed(2));
     }
   }, [figures.figure1a, figures.figure1b, figures.figure1c]);
 
   React.useEffect(() => {
-    if(
-       ValidateNumber(figures.figure2a) &&
-       ValidateNumber(figures.figure2b) &&
-       ValidateNumber(figures.figure2c) &&
-       ValidateNumber(figures.figure2d) &&
-       ValidateNumber(figures.figure2e) &&
-       ValidateNumber(figures.figure2f)
-       ) {
+    if (
+      ValidateNumber(figures.figure2a) &&
+      ValidateNumber(figures.figure2b) &&
+      ValidateNumber(figures.figure2c) &&
+      ValidateNumber(figures.figure2d) &&
+      ValidateNumber(figures.figure2e) &&
+      ValidateNumber(figures.figure2f)
+    ) {
       const sideA = Number(figures.figure2a);
       const sideB = Number(figures.figure2b);
       const sideC = Number(figures.figure2c);
@@ -43,11 +41,20 @@ export default function FigureScreen() {
       const sideE = Number(figures.figure2e);
       const sideF = Number(figures.figure2f);
 
-      const r1 = Math.sqrt(Math.pow(((sideA - sideC) + sideE), 2) + Math.pow((sideB + sideD + sideF), 2));
+      const r1 = Math.sqrt(
+        Math.pow(sideA - sideC + sideE, 2) + Math.pow(sideB + sideD + sideF, 2)
+      );
 
-      figures.setFigure2GData('' + r1.toFixed(2));
+      figures.setFigure2GData("" + r1.toFixed(2));
     }
-  }, [figures.figure2a, figures.figure2b, figures.figure2c, figures.figure2d, figures.figure2e, figures.figure2f]);
+  }, [
+    figures.figure2a,
+    figures.figure2b,
+    figures.figure2c,
+    figures.figure2d,
+    figures.figure2e,
+    figures.figure2f,
+  ]);
 
   React.useEffect(() => {
     if (
@@ -59,9 +66,9 @@ export default function FigureScreen() {
       const sideC = Number(figures.figure3c);
       const sideD = Number(figures.figure3d);
 
-      const r1 = pythagorean((sideD - sideB), sideC);
+      const r1 = pythagorean(sideD - sideB, sideC);
 
-      figures.setFigure3AData('' + r1.toFixed(2));
+      figures.setFigure3AData("" + r1.toFixed(2));
     }
   }, [figures.figure3b, figures.figure3c, figures.figure3d]);
 
@@ -82,24 +89,26 @@ export default function FigureScreen() {
       {/* Figure 1 */}
       <View>
         <View style={styles.figuresTopOptions}>
-          <Text style={styles.boldStyle}>Figure 1 {figures.figure1 ? "True" : "False"}</Text>
+          <Text style={styles.boldStyle}>Figure 1</Text>
           <Pressable
             style={{ borderColor: "#000000", borderRadius: 4 }}
             onPress={() => {
-              console.log("Pressed");
+              figures.setFiguresData(1, !figures.figure1);
             }}
           >
             <Text style={styles.boldStyle}>Pick Figure</Text>
           </Pressable>
         </View>
         <View style={styles.imageContainer}>
-          <Image
-            source={require("../../assets/figures/Figure1.png")}
-          />
+          <Image source={require("../../assets/figures/Figure1.png")} />
         </View>
         <Text>Measurements</Text>
         <Text>a</Text>
-        <TextInput style={styles.input} value={figures.figure1a.toString()} editable={false} />
+        <TextInput
+          style={styles.input}
+          value={figures.figure1a.toString()}
+          editable={false}
+        />
 
         <Text>b</Text>
         <TextInput
@@ -121,48 +130,61 @@ export default function FigureScreen() {
       <View style={{ flex: 1 }}>
         <View style={styles.figuresTopOptions}>
           <Text style={styles.boldStyle}>Figure 2</Text>
-          <Pressable style={{ borderColor: "#FFFFFF", borderRadius: 4 }}>
+          <Pressable
+            onPress={() => {
+              figures.setFiguresData(2, !figures.figure2);
+            }}
+            style={{ borderColor: "#FFFFFF", borderRadius: 4 }}
+          >
             <Text style={styles.boldStyle}>Pick Figure</Text>
           </Pressable>
         </View>
         <View style={styles.imageContainer}>
-          <Image
-            source={require("../../assets/figures/Figure2.png")}
-          />
+          <Image source={require("../../assets/figures/Figure2.png")} />
         </View>
         <Text>Measurements</Text>
-        <Text>a</Text><TextInput
+        <Text>a</Text>
+        <TextInput
           style={styles.input}
           onChangeText={figures.setFigure2AData}
           value={figures.figure2a.toString()}
         />
-        <Text>b</Text><TextInput
+        <Text>b</Text>
+        <TextInput
           style={styles.input}
           onChangeText={figures.setFigure2BData}
           value={figures.figure2b.toString()}
         />
-        <Text>c</Text><TextInput
+        <Text>c</Text>
+        <TextInput
           style={styles.input}
           onChangeText={figures.setFigure2CData}
           value={figures.figure2c.toString()}
         />
-        <Text>d</Text><TextInput
+        <Text>d</Text>
+        <TextInput
           style={styles.input}
           onChangeText={figures.setFigure2DData}
           value={figures.figure2d.toString()}
         />
-        <Text>e</Text><TextInput
+        <Text>e</Text>
+        <TextInput
           style={styles.input}
           onChangeText={figures.setFigure2EData}
           value={figures.figure2e.toString()}
         />
-        <Text>f</Text><TextInput
+        <Text>f</Text>
+        <TextInput
           style={styles.input}
           onChangeText={figures.setFigure2FData}
           value={figures.figure2f.toString()}
         />
         <Text>g</Text>
-        <TextInput style={styles.input} value={figures.figure2g.toString()} editable={false} />
+        <TextInput
+          style={styles.input}
+          value={figures.figure2g.toString()}
+          editable={false}
+        />
       </View>
       {/* End Figure 2 */}
 
@@ -170,7 +192,12 @@ export default function FigureScreen() {
       <View style={{ flex: 1 }}>
         <View style={styles.figuresTopOptions}>
           <Text>Figure</Text>
-          <Pressable style={{ borderColor: "#000000", borderRadius: 4 }}>
+          <Pressable
+            onPress={() => {
+              figures.setFiguresData(3, !figures.figure3);
+            }}
+            style={{ borderColor: "#000000", borderRadius: 4 }}
+          >
             <Text>Pick Figure</Text>
           </Pressable>
         </View>
@@ -209,14 +236,18 @@ export default function FigureScreen() {
 }
 
 const styles = StyleSheet.create({
-  figuresTopOptions: { paddingTop: 10, flexDirection: "row", justifyContent: "space-around" },
+  figuresTopOptions: {
+    paddingTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
   imageContainer: {
     paddingTop: 30,
     paddingBottom: 30,
     alignItems: "center",
     justifyContent: "center",
   },
-  boldStyle: {fontWeight: "bold", fontSize: 20},
+  boldStyle: { fontWeight: "bold", fontSize: 20 },
   figuresImages: {
     height: 200,
     width: win.width / 2,
